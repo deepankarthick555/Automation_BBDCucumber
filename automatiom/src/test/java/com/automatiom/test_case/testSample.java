b2f6b5e9e80d8bc6b42607eb506e8fb3f929048e
Deepan <Deepan@DESKTOP-ELJASUQ>
2023-11-14 18:10:05 +0530

    Data Driven Testing

---
+++ automatiom/src/test/java/com/automatiom/test_case/testSample.java
@@ -0,0 +1,20 @@
+package com.automatiom.test_case;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class testSample {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		dataDriven d = new dataDriven();
+		
+		ArrayList<String> data = d.getData("Purchase");
+		System.out.println(data.get(0));
+		System.out.println(data.get(1));
+		System.out.println(data.get(2));
+		System.out.println(data.get(3));
+	}
+
+}
